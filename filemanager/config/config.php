9e0518b1c82291a0ba93889a02cadde12ab3b61d
Dominic Gisler <domgisler@gmail.com>
2019-05-16 18:38:06 +0200

    update

--- filemanager/config/config.php
+++ filemanager/config/config.php
@@ -76,7 +76,7 @@ $config = array(
     | with start and final /
     |
     */
-    'upload_dir' => '/source/',
+    'upload_dir' => '/uploads/',
     /*
     |--------------------------------------------------------------------------
     | relative path from filemanager folder to upload folder
@@ -85,7 +85,7 @@ $config = array(
     | with final /
     |
     */
-    'current_path' => '../source/',
+    'current_path' => '../../uploads/',
 
     /*
     |--------------------------------------------------------------------------
@@ -96,7 +96,7 @@ $config = array(
     | DO NOT put inside upload folder
     |
     */
-    'thumbs_base_path' => '../thumbs/',
+    'thumbs_base_path' => '../../thumbs/',
 
     /*
     |--------------------------------------------------------------------------
